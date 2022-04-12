f2bab67678b886a08317cf78b200e71d7dad1d12
Gerry-Geraldy <whateveritakes10@gmail.com>
2022-04-12 18:47:42 +0800

    Saat Klik background color akan muncul

--- script.js
+++ script.js
@@ -3,3 +3,11 @@ document.onmousemove = (event) => {
     kotak.style.left = event.clientX - (kotak.clientWidth/2) + 'px'; 
     kotak.style.top  = event.clientY - (kotak.clientHeight/2)+ 'px';
 }
+
+
+function ubahWarna() {
+    kotak.style.backgroundColor = 'lightblue';
+}
+
+const div = document.querySelector('#kotak');
+div.onclick = ubahWarna;
